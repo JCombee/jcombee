e7e4d6266600a8d0f54473ccf0d3283137068cb4
Jerke Combee <jerke@elgentos.nl>
2020-12-31 17:10:56 +0100

    Implemented blog posts

---
+++ resources/content/collections/articles/blog-post-a.md
@@ -0,0 +1,8 @@
+---
+title: "Test Blog A"
+tag: TEST POST
+post_date: "2021-01-01 00:00:00"
+read_time: 1
+---
+
+This is a test blog post.
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,14 @@ Route::get('/blog', function () {
     return view('blog');
 })->name('blog.index');
 
+Route::get('/blog/{slug}', function ($slug) {
+    $article = \AloiaCms\Models\Article::find($slug);
+    if (!$article) {
+        return response('', 404);
+    }
+    return view('post', compact(['article']));
+})->name('blog.show');
+
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact.index');
