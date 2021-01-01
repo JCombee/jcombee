f3cb2c4e64c5698bf2adc1bb5ad8974e78f1b179
Jerke Combee <jerke@elgentos.nl>
2021-01-01 14:57:51 +0100

    Load cover through assets

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,7 +17,7 @@
 
 <!--Header-->
 <div class="w-full m-0 p-0 bg-cover bg-bottom"
-     style="background-image:url('cover.jpg'); height: 60vh; max-height:460px;">
+     style="background-image:url({{ asset('cover.jpg') }}); height: 60vh; max-height:460px;">
     <div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
         <!--Title-->
         <p class="text-white font-extrabold text-3xl md:text-5xl">
