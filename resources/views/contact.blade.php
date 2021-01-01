abc214c39d9ba318813a3bb7b225dbdfbae6ed75
Jerke Combee <jerke@elgentos.nl>
2021-01-01 14:30:19 +0100

    Update formatting of contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,21 +13,42 @@
                     <div class="grid grid-cols-6">
                         <div class="col-span-6 sm:col-span-3">
                             <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
-                            <input type="text" name="first_name" id="first_name" autocomplete="given-name"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                            <input
+                                type="text"
+                                name="first_name"
+                                id="first_name"
+                                autocomplete="given-name"
+                                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                required
+                                aria-required
+                            />
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
                             <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
-                            <input type="text" name="last_name" id="last_name" autocomplete="family-name"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                            <input
+                                type="text"
+                                name="last_name"
+                                id="last_name"
+                                autocomplete="family-name"
+                                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                required
+                                aria-required
+                            />
                         </div>
 
                         <div class="col-span-6">
                             <label for="email_address" class="block text-sm font-medium text-gray-700">Email
                                 address</label>
-                            <input type="text" name="email_address" id="email_address" autocomplete="email"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                            <input
+                                type="text"
+                                name="email_address"
+                                id="email_address"
+                                autocomplete="email"
+                                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                required
+                                aria-required
+                            />
                         </div>
 
                         <div class="col-span-6">
@@ -40,14 +61,18 @@
                                     name="message"
                                     rows="3"
                                     class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
+                                    required
+                                    aria-required
                                 ></textarea>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button type="submit"
-                            class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <button
+                        type="submit"
+                        class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
                         Save
                     </button>
                 </div>
